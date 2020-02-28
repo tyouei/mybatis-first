b78b6e847d77ef6f28239fd9ebeadec6396c64df
haotian0911 <tyouei0911@yahoo.co.jp>
2020-02-28 22:35:36 +0900

    20200228 ユーザＤＢの更新・削除の追加

--- mybatis-first/src/cn/itcast/mybatis/test/MybatisTest.java
+++ mybatis-first/src/cn/itcast/mybatis/test/MybatisTest.java
@@ -8,14 +8,16 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import cn.itcast.mybatis.po.User;
 
 public class MybatisTest {
+
 	private SqlSessionFactory sqlSessionFactory = null;
 
-	@Test
+	@Before
 	public void init() throws Exception {
 		// 1. SqlSessionFactoryBuilderを作成
 		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
@@ -26,6 +28,11 @@ public class MybatisTest {
 		// 3. sqlSessionFactoryを作成
 		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
 
+
+	}
+
+	@Test
+	public void testSelectOne() throws Exception{
 		//sqlsessionを作成
 		SqlSession sqlSession=this.sqlSessionFactory.openSession();
 
@@ -40,13 +47,13 @@ public class MybatisTest {
 	@Test
 	public void testSelectUserByUserName() throws Exception {
 		// 1. SqlSessionFactoryBuilderを作成
-		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+//		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
 
 		// 2. SqlMapConfig.xmlを読み込む
-		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+//		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
 
 		// 3. sqlSessionFactoryを作成
-		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+//		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
 
 		//sqlsessionを作成
 		SqlSession sqlSession=this.sqlSessionFactory.openSession();
@@ -62,16 +69,15 @@ public class MybatisTest {
 	}
 
 	@Test
-
 	public void insertUser() throws Exception{
 		// 1. SqlSessionFactoryBuilderを作成
-		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
-
-		// 2. SqlMapConfig.xmlを読み込む
-		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
-
-		// 3. sqlSessionFactoryを作成
-		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+//		SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+//
+//		// 2. SqlMapConfig.xmlを読み込む
+//		InputStream inputStream = Resources.getResourceAsStream("SqlMapConfig.xml");
+//
+//		// 3. sqlSessionFactoryを作成
+//		this.sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
 
 		//sqlsessionを作成
 		SqlSession sqlSession=this.sqlSessionFactory.openSession();
@@ -86,11 +92,49 @@ public class MybatisTest {
 		user.setAddress("千葉県千葉市美浜区打たせ");
 		//登録
 		sqlSession.insert("insertUser",user);
+
 		//コミット
 		sqlSession.commit();
-
+        //登録されたユーザＩＤを出力する。
+		System.out.println(user.getId());
 		//sqlSession解除
 		sqlSession.close();
 	}
 
+	@Test
+	public void testUpdateUser() throws Exception{
+
+		// ユーザ登録の引数インスタンスの作成
+				User user=new User();
+				// userに以下の引数を代入
+				user.setId(28);
+				//username
+				user.setUsername("千葉　寛子");
+				user.setBirthday(new Date());
+				user.setSex("1");
+				user.setAddress("千葉県千葉市美浜区高洲1");
+
+				SqlSession sqlSession=sqlSessionFactory.openSession();
+				sqlSession.update("updateUserByUserId", user);
+
+				//コミット
+				sqlSession.commit();
+				//sqlSession解除
+				sqlSession.close();
+
+	}
+
+	@Test
+	public void testDeleteUser() throws Exception{
+		User user=new User();
+		user.setId(28);
+
+		SqlSession sqlSession=sqlSessionFactory.openSession();
+		sqlSession.delete("deleteUserByUserId", user);
+
+		sqlSession.commit();
+		sqlSession.close();
+
+
+	}
 }
